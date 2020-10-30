5d5bafacf64a12b30b9fb18fd5ee448ae2e93f53
SARAGOCA Glaydson <glaydson.saragoca@gmail.com>
2020-10-30 00:52:49 -0300

    add pokeapi service

---
+++ src/services/pokeapi.ts
@@ -0,0 +1,59 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2',
+});
+
+
+const pokeapi = {
+  getPokemons: async (page: number) => (
+    new Promise((resolve, reject) => {
+      const limit = 20;
+      api.get(`/pokemon/?offset=${(page - 1) * limit}&limit=${limit}`)
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    })
+  ),
+
+  getPokemon: async (name: number) => (
+    new Promise((resolve, reject) => {
+      api.get(`/pokemon/${name}`)
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    })
+  ),
+
+  getSpecie: async (name: number) => (
+    new Promise((resolve, reject) => {
+      api.get(`/pokemon-species/${name}`)
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    })
+  ),
+
+  getAbility: async (name: number) => (
+    new Promise((resolve, reject) => {
+      api.get(`/ability/${name}`)
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    })
+  ),
+};
+
+export default pokeapi;
